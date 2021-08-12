6381de249cbedb8f10b85a83c102c88dc1127751
ianito <iaan.developer@gmail.com>
2021-08-12 17:22:43 -0300

    Changed user router

--- src/routes/Users.ts
+++ src/routes/Users.ts
@@ -14,15 +14,36 @@ import UserModel from '../models/UserModel';
 import { ObjectId, Types, SchemaTypes } from 'mongoose';
 
 const userRouter = Router();
-
-userRouter
-  .route('/')
-  .get(checkJWT, async (req: Request, res: Response) => {
-    let errorMessage: IErrorUser;
-    const showUsersDisabled = req.params.disabled ? true : false;
-    listUsers({ disabled: showUsersDisabled })
-      .then((users) => {
-        res.json(users);
+userRouter.get(checkJWT, async (req: Request, res: Response) => {
+  let errorMessage: IErrorUser;
+  const showUsersDisabled = req.params.disabled ? true : false;
+  listUsers({ disabled: showUsersDisabled })
+    .then((users) => {
+      res.json(users);
+    })
+    .catch((err) => {
+      errorMessage = {
+        type: err.type,
+        message: err.message,
+        status: err.status || 500,
+      };
+      return res.status(<number>errorMessage.status).json(errorMessage);
+    });
+});
+userRouter.post(async (req: Request, res: Response) => {
+  let errorMessage: IErrorUser;
+  const { name, password, email }: IUser = req.body;
+  if (!name || !password || !email) {
+    errorMessage = {
+      type: ERROR_USERS_ENUMS.DATA_INVALID,
+      message: 'Data invalid!',
+      status: 400,
+    };
+    return res.status(<number>errorMessage.status).json(errorMessage);
+  } else {
+    saveUser(req.body)
+      .then((user) => {
+        return res.status(201).json(user);
       })
       .catch((err) => {
         errorMessage = {
@@ -32,32 +53,8 @@ userRouter
         };
         return res.status(<number>errorMessage.status).json(errorMessage);
       });
-  })
-  .post(async (req: Request, res: Response) => {
-    let errorMessage: IErrorUser;
-    const { name, password, email }: IUser = req.body;
-    if (!name || !password || !email) {
-      errorMessage = {
-        type: ERROR_USERS_ENUMS.DATA_INVALID,
-        message: 'Data invalid!',
-        status: 400,
-      };
-      return res.status(<number>errorMessage.status).json(errorMessage);
-    } else {
-      saveUser(req.body)
-        .then((user) => {
-          return res.status(201).json(user);
-        })
-        .catch((err) => {
-          errorMessage = {
-            type: err.type,
-            message: err.message,
-            status: err.status || 500,
-          };
-          return res.status(<number>errorMessage.status).json(errorMessage);
-        });
-    }
-  });
+  }
+});
 
 userRouter.route('/token').post(async (req: Request, res: Response) => {
   let errorMessage: IErrorUser;
